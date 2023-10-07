208c24a77ea1fddeec7ef218d3817ec98c1ea127
germancin <elmaildegerman@gmail.com>
2023-10-07 18:32:50 -0400

    update

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-// const Gpio = require('pigpio').Gpio;
+const Gpio = require('pigpio').Gpio;
 // const led = new Gpio(17, { mode: Gpio.OUTPUT });
 
 const app = express();
